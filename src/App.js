2f7b5bc980e230fd21afc5618edb0e38ee601460
Mehedi4467 <mehedi446768@gmail.com>
2022-08-20 22:01:02 +0600

    update react dom and react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Compress from "react-image-file-resizer";
+import { useState } from 'react';
 function App() {
+
+
+  const imgHandel = async (event) => {
+    event.preventDefault();
+    let formData = new FormData();
+
+    const imageData = event.target.img.files[0];
+    const secondImageData = event.target.secondImg.files[0] || [];
+    formData.append('primaryImage', imageData);
+    for (let i = 0; i < secondImageData.length; i++) {
+      formData.append(`secondImage`, secondImageData[i]);
+    };
+
+    // const productName = event.target.name.value;
+    await fetch('http://localhost:5000/product', {
+      method: "POST",
+
+      body: formData,
+
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.acknowledged) {
+          console.log(data)
+        }
+      })
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>React Image File Resizer</h2>
+      <br />
+      <form onSubmit={imgHandel}>
+        <input type="text" name='name' />
+        <input
+          type="file"
+          accept="image/*"
+          name='img'
+        // onChange={onFileResize}
+        />
+        <input
+          type="file"
+          accept="image/*"
+          // onChange={onFileResize}
+          name='secondImg'
+          multiple
+        />
+
+        <button>Submit</button>
+
+      </form>
+
     </div>
   );
 }
